4813013c170f6b4c7b05dc6cd0628ac5a4e39f68
Dominik Nitschmann <dominik.nitschmann@liebherr.com>
2025-05-06 07:28:55 +0200

    Add second bucket pr

--- environments/tfenterprise-dev/main.tf
+++ environments/tfenterprise-dev/main.tf
@@ -3,3 +3,9 @@ module "s3" {
 
     bucket_name = "tfenterprise-test-dn-${var.environment}"
 }
+
+module "s3_second_bucket" {
+    source = "../../modules/s3"
+
+    bucket_name = "tfenterprise2-test-dn-${var.environment}"
+}
